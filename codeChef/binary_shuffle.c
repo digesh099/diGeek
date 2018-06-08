69d804f69ab59bf4d85e68f3710f019261b7237f
Digesh Patel <digesh.patel@volansystech.com>
2018-06-08 19:36:16 +0530

    supporting 10^18 range if A,B

--- codeChef/binary_shuffle.c
+++ codeChef/binary_shuffle.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<inttypes.h>
+#include<math.h>
 
 /* Function to get no of set bits in binary
    representation of positive integer n */
-unsigned int countSetBits(uint8_t n)
+unsigned int countSetBits(uint64_t n)
 {
     unsigned int count = 0;
     while (n)
@@ -15,10 +16,19 @@ unsigned int countSetBits(uint8_t n)
     return count;
 }
 
-unsigned power(unsigned base, unsigned degree)
+uint64_t power(uint64_t base, uint64_t powerRaised)
 {
-    unsigned result = 1;
-    unsigned term = base;
+    if (powerRaised != 0)
+        return (base*power(base, powerRaised-1));
+    else
+        return 1;
+}
+
+#if 0
+uint64_t power(unsigned base, unsigned degree)
+{
+    uint64_t result = 1;
+    uint64_t term = base;
     while (degree)
     {
         if (degree & 1)
@@ -28,47 +38,55 @@ unsigned power(unsigned base, unsigned degree)
     }
     return result;
 }
+#endif
 
-uint8_t binShuffleEquel(uint8_t b)
+uint64_t binShuffleEquel(uint64_t b)
 {
     return b-1;
 }
 
-uint8_t binShuffleGreater(uint8_t srcCount, uint8_t dstCount)
+uint64_t binShuffleGreater(uint8_t srcCount, uint8_t dstCount)
 {
-    uint8_t diff, tmp1, tmp2, tmp3, tmp4;
+    uint8_t diff = 0, tmp2 = 0, tmp4 = 0;
+    uint64_t tmp1 = 0, tmp3 = 0;
     diff = srcCount - dstCount;
     tmp1 = power(2,diff+1)-1;
     tmp2 = srcCount - countSetBits(tmp1);
     tmp3 = power(2,tmp2)-1;
-    tmp4 = 8 - countSetBits(tmp3);
+    tmp4 = 64 - countSetBits(tmp3);
     tmp3 <<= tmp4;
 
     return (tmp1 | tmp3);
 }
 
-uint8_t binShuffleLesser(uint8_t src)
+uint64_t binShuffleLesser(uint8_t srcCount)
 {
-    uint8_t tmp;
-    tmp = power(2,src)-1;
+    uint64_t tmp;
+    tmp = power(2,srcCount)-1;
     return (tmp <<= 1);
 }
 
 void main()
 {
-    uint8_t a = 0, b = 0, aCount = 0, bMinusOneCount = 0, count = 0;
+    uint64_t a = 0, b = 0;
+    uint8_t aCount = 0, bMinusOneCount = 0, count = 0;
     uint32_t numOfOperation = 0;
     scanf("%d", &numOfOperation);
 
     while(numOfOperation--)
     {
-        scanf("%3" SCNu8 "%3" SCNu8, &a, &b);
-        printf("%3" PRIu8 " %3" PRIu8 "\n", a, b);
+        scanf("%" SCNu64 "%" SCNu64, &a, &b);
+        //printf("%" PRIu64 " %" PRIu64 "\n", a, b);
+        bMinusOneCount = countSetBits(b-1);
+        if(a!=b && bMinusOneCount == 64)
+        {
+            printf("%d\n", -1);
+            continue;
+        }
         // 1. prefix zeroes are already added since unsigned int decimal is taken
         while(a!=b)
         {
             aCount = countSetBits(a);
-            bMinusOneCount = countSetBits(b-1);
             if(aCount == bMinusOneCount) // final condition to reaching B
             {
                 // shuffle operation
