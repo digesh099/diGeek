396e93c4b2ac49bf714ced00e142156e55480508
Digesh Patel <digesh.patel@volansystech.com>
2018-06-08 13:22:39 +0530

    part-1 bin shuffle. a,b <= 2^7

--- codeChef/binary_shuffle.c
+++ codeChef/binary_shuffle.c
@@ -29,40 +29,67 @@ unsigned power(unsigned base, unsigned degree)
     return result;
 }
 
+uint8_t binShuffleEquel(uint8_t b)
+{
+    return b-1;
+}
+
+uint8_t binShuffleGreater(uint8_t srcCount, uint8_t dstCount)
+{
+    uint8_t diff, tmp1, tmp2, tmp3, tmp4;
+    diff = srcCount - dstCount;
+    tmp1 = power(2,diff+1)-1;
+    tmp2 = srcCount - countSetBits(tmp1);
+    tmp3 = power(2,tmp2)-1;
+    tmp4 = 8 - countSetBits(tmp3);
+    tmp3 <<= tmp4;
+
+    return (tmp1 | tmp3);
+}
+
+uint8_t binShuffleLesser(uint8_t src)
+{
+    uint8_t tmp;
+    tmp = power(2,src)-1;
+    return (tmp <<= 1);
+}
+
 void main()
 {
-    uint8_t a = 0, b = 0, aCount = 0, bMinusOneCount = 0, count = 0, tmp = 0;
+    uint8_t a = 0, b = 0, aCount = 0, bMinusOneCount = 0, count = 0;
     uint32_t numOfOperation = 0;
     scanf("%d", &numOfOperation);
     //printf("num of operation is : %d\n", numOfOperation);
 
     while(numOfOperation--)
     {
-        scanf("%2" SCNu8 "%2" SCNu8, &a, &b);
+        scanf("%3" SCNu8 "%3" SCNu8, &a, &b);
         //printf("num of operation is : %d\n", numOfOperation);
-        printf("%1" PRIu8 " %1" PRIu8 "\n", a, b);
+        printf("%3" PRIu8 " %3" PRIu8 "\n", a, b);
         // 1. prefix zeroes are already added since unsigned int decimal is taken
         while(a!=b)
         {
-            count++;
             aCount = countSetBits(a);
             bMinusOneCount = countSetBits(b-1);
             if(aCount == bMinusOneCount) // final condition to reaching B
             {
                 // shuffle operation
-                a = b-1;
-                // addition operation
-                ++a;
+                a = binShuffleEquel(b);
             }
             else if(aCount > bMinusOneCount)
             {
-                a &= a-1;
+                // shuffle operation
+                a = binShuffleGreater(aCount, bMinusOneCount);
             }
             else if(aCount < bMinusOneCount)
             {
-                a = power(2,aCount)-1;
-                a <<= 1;
+                // shuffle operation
+                a = binShuffleLesser(aCount);
             }
+
+            // +1 operation    
+            ++a;
+            ++count;
 
         }
         //printf("count to reach A --> B is %d\n", count);
