744dcb5c84eb4796d3fe93085bd9c8845e6d5446
Digesh Patel <digesh.patel@volansystech.com>
2018-06-09 11:38:50 +0530

    final submitted code

--- codeChef/binary_shuffle.c
+++ codeChef/binary_shuffle.c
@@ -5,7 +5,7 @@
 
 /* Function to get no of set bits in binary
    representation of positive integer n */
-unsigned int countSetBits(uint64_t n)
+unsigned int countSetBits(unsigned long long int n)
 {
     unsigned int count = 0;
     while (n)
@@ -16,7 +16,7 @@ unsigned int countSetBits(uint64_t n)
     return count;
 }
 
-uint64_t power(uint64_t base, uint64_t powerRaised)
+unsigned long long int power(unsigned long long int base, unsigned long long int powerRaised)
 {
     if (powerRaised != 0)
         return (base*power(base, powerRaised-1));
@@ -24,31 +24,15 @@ uint64_t power(uint64_t base, uint64_t powerRaised)
         return 1;
 }
 
-#if 0
-uint64_t power(unsigned base, unsigned degree)
-{
-    uint64_t result = 1;
-    uint64_t term = base;
-    while (degree)
-    {
-        if (degree & 1)
-            result *= term;
-        term *= term;
-        degree = degree >> 1;
-    }
-    return result;
-}
-#endif
-
-uint64_t binShuffleEquel(uint64_t b)
+unsigned long long int binShuffleEquel(unsigned long long int b)
 {
     return b-1;
 }
 
-uint64_t binShuffleGreater(uint8_t srcCount, uint8_t dstCount)
+unsigned long long int binShuffleGreater(int srcCount, int dstCount)
 {
-    uint8_t diff = 0, tmp2 = 0, tmp4 = 0;
-    uint64_t tmp1 = 0, tmp3 = 0;
+    unsigned int diff = 0, tmp2 = 0, tmp4 = 0;
+    unsigned long long int tmp1 = 0, tmp3 = 0;
     diff = srcCount - dstCount;
     tmp1 = power(2,diff+1)-1;
     tmp2 = srcCount - countSetBits(tmp1);
@@ -59,24 +43,24 @@ uint64_t binShuffleGreater(uint8_t srcCount, uint8_t dstCount)
     return (tmp1 | tmp3);
 }
 
-uint64_t binShuffleLesser(uint8_t srcCount)
+unsigned long long int binShuffleLesser(int srcCount)
 {
-    uint64_t tmp;
+    unsigned long long int tmp;
     tmp = power(2,srcCount)-1;
     return (tmp <<= 1);
 }
 
 void main()
 {
-    uint64_t a = 0, b = 0;
-    uint8_t aCount = 0, bMinusOneCount = 0, count = 0;
-    uint32_t numOfOperation = 0;
+    unsigned long long int a = 0, b = 0;
+    unsigned int aCount = 0, bMinusOneCount = 0, count = 0;
+    unsigned int numOfOperation = 0;
+    unsigned short int invalidFlag = 0;
     scanf("%d", &numOfOperation);
 
     while(numOfOperation--)
     {
-        scanf("%" SCNu64 "%" SCNu64, &a, &b);
-        //printf("%" PRIu64 " %" PRIu64 "\n", a, b);
+        scanf("%lld %lld", &a, &b);
         bMinusOneCount = countSetBits(b-1);
         if(a!=b && bMinusOneCount == 64)
         {
@@ -87,28 +71,33 @@ void main()
         while(a!=b)
         {
             aCount = countSetBits(a);
-            if(aCount == bMinusOneCount) // final condition to reaching B
+            // 2. shuffle binary. reaching s
+            if(aCount == bMinusOneCount)
             {
-                // shuffle operation
                 a = binShuffleEquel(b);
             }
             else if(aCount > bMinusOneCount)
             {
-                // shuffle operation
+                if(bMinusOneCount == 0)
+                {
+                    printf("%d\n", -1);
+                    invalidFlag = 1;
+                    break;
+                }
                 a = binShuffleGreater(aCount, bMinusOneCount);
             }
             else if(aCount < bMinusOneCount)
             {
-                // shuffle operation
                 a = binShuffleLesser(aCount);
             }
 
-            // +1 operation    
+            // 3. s+1 operation
             ++a;
             ++count;
 
         }
-        printf("%d\n", count);
+        if(!invalidFlag) printf("%d\n", count);
+        invalidFlag = 0;
         count = 0;
     }
     return;
